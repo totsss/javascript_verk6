fe4e3496a6b65d9701442c1ad07cf84e05b55991
totsss <axetra@hotmail.com>
2018-11-23 09:38:36 +0000

    Add files via upload

---
+++ hluti2/image.js
@@ -0,0 +1,12 @@
+img = [
+	{name:'dodge charger' , src: "./myndir/charger.jpg" , tags:"white, sport, 2018", type:'dodge'},
+	{name:'honda jazz' , src: "./myndir/charger.jpg" , tags:"blue, hatchback, 2003", type:'honda'},
+	{name:'honda s2000' , src: "./myndir/charger.jpg" , tags:"gray, sport, 2002", type:'honda'},
+	{name:'honda nrx' , src: "./myndir/charger.jpg" , tags:"white, sport, 2018", type:'honda'},
+	{name:'honda civic' , src: "./myndir/charger.jpg" , tags:"white, sport, 2018", type:'dodge'},
+	{name:'dodge charger' , src: "./myndir/charger.jpg" , tags:"white, sport, 2018", type:'dodge'},
+	// {name:'dodge charger' , src: "./myndir/charger.jpg" , tags:"white, sport, 2018", type:'dodge'},
+	// {name:'dodge charger' , src: "./myndir/charger.jpg" , tags:"white, sport, 2018", type:'dodge'},
+	// {name:'dodge charger' , src: "./myndir/charger.jpg" , tags:"white, sport, 2018", type:'dodge'},
+	// {name:'dodge charger' , src: "./myndir/charger.jpg" , tags:"white, sport, 2018", type:'dodge'},
+]
---
+++ hluti2/verkefni_6_seinnipartur.js
@@ -0,0 +1,5 @@
+var myImage = new Image(100, 200);
+
+myImage.src = 'picture.jpg';
+
+document.body.appendChild(myImage);
