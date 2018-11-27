262a2cbc447742c91ec08c99be122bb2b25ad881
totsss <axetra@hotmail.com>
2018-11-27 12:28:06 +0000

    Add files via upload

--- hluti2/image.js
+++ hluti2/image.js
@@ -1,10 +1,10 @@
-img = [
+imgs = [
 	{name:'dodge charger' , src: "./myndir/charger.jpg" , tags:"white, sport, 2018", type:'dodge'},
-	{name:'honda jazz' , src: "./myndir/charger.jpg" , tags:"blue, hatchback, 2003", type:'honda'},
-	{name:'honda s2000' , src: "./myndir/charger.jpg" , tags:"gray, sport, 2002", type:'honda'},
-	{name:'honda nrx' , src: "./myndir/charger.jpg" , tags:"white, sport, 2018", type:'honda'},
-	{name:'honda civic' , src: "./myndir/charger.jpg" , tags:"white, sport, 2018", type:'dodge'},
-	{name:'dodge charger' , src: "./myndir/charger.jpg" , tags:"white, sport, 2018", type:'dodge'},
+	{name:'honda jazz' , src: "./myndir/jazz.jpeg" , tags:"blue, hatchback, 2003", type:'honda'},
+	{name:'honda s2000' , src: "./myndir/s2000.jpeg" , tags:"gray, sport, 2002", type:'honda'},
+	{name:'honda nsx' , src: "./myndir/nsx.jpg" , tags:"white, sport, 2018", type:'honda'},
+	{name:'honda civic' , src: "./myndir/civicr.jpg" , tags:"white, sport, 2018", type:'dodge'},
+	{name:'dodge hellcat' , src: "./myndir/hellcat.jpg" , tags:"red, sport, 2018", type:'dodge'},
 	// {name:'dodge charger' , src: "./myndir/charger.jpg" , tags:"white, sport, 2018", type:'dodge'},
 	// {name:'dodge charger' , src: "./myndir/charger.jpg" , tags:"white, sport, 2018", type:'dodge'},
 	// {name:'dodge charger' , src: "./myndir/charger.jpg" , tags:"white, sport, 2018", type:'dodge'},
--- hluti2/verkefni_6_seinnipartur.js
+++ hluti2/verkefni_6_seinnipartur.js
@@ -1,5 +1,18 @@
-var myImage = new Image(100, 200);
 
-myImage.src = 'picture.jpg';
 
-document.body.appendChild(myImage);
+myndir = [];
+
+imgs.forEach(function(img){
+	let imgs = new Image();
+	imgs.dataset.name=img.name;
+	imgs.src = img.src;
+	imgs.dataset.tags = img.tags;
+	imgs.dataset.type=img.type;
+	myndir.push(imgs)
+});
+ 
+let myndasafn = document.getElementById('gallery');
+
+myndir.forEach(img => {
+myndasafn.appendChild(img);	//Sýnir allar myndinar.
+});
